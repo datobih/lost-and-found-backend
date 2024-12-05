e8b12b2e1b51a75ed6728ab72c0b150118c38e55
lilesanmi1357 <dayodele89@outlook.com>
2024-12-05 17:30:39 +0100

    update

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -111,4 +111,4 @@ class ResendEmailSerializer(serializers.Serializer):
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id','first_name','last_name','email','profile_image']
+        fields = ['id','first_name','last_name','email','profile_image','phone_number']
