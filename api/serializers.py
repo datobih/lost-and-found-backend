babad7235db0dcf9f6ac014782762b5b3b14d367
lilesanmi1357 <dayodele89@outlook.com>
2024-12-06 00:20:22 +0100

    update

--- api/serializers.py
+++ api/serializers.py
@@ -2,9 +2,13 @@ from rest_framework import serializers
 from .models import Item
 
 class ItemSerializer(serializers.ModelSerializer):
+    contact = serializers.CharField(source = 'posted_by.phone_number',read_only=True)
+
     class Meta:
         model = Item
         fields = '__all__'
+
+
 
 
 class IDSerialiser(serializers.Serializer):
